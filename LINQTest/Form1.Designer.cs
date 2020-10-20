4bae1937728d6277c8c9bc9e3d88d502c554eb01
Андрей Беляков <ttxiom@gmail.com>
2020-10-20 16:56:58 +0500

    linq-test

---
+++ LINQTest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LINQTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(31, 30);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(131, 102);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtData.Location = new System.Drawing.Point(188, 30);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(410, 34);
+            this.txtData.TabIndex = 1;
+            this.txtData.Text = "0 1 2 100 4 5 66 99";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtResult.Location = new System.Drawing.Point(188, 98);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(410, 34);
+            this.txtResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ LINQTest/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = txtData.Text;
+
+            char[] sep = new char[] { ' ', '|', '-' };
+            //string[] arr = s.Split(sep);
+            //arr = arr.Skip(3).ToArray();
+            //txtResult.Text = String.Join(" ", arr);
+
+            //txtResult.Text = txtData
+            //    .Text
+            //    .Split(sep)
+            //    .Select(item => Convert.ToInt32(item))
+            //    .Sum()
+            //    .ToString();
+
+            //txtResult.Text = string.Join(" ", txtData
+            //    .Text
+            //    .Split(sep)
+            //    .Select(item => Convert.ToInt32(item))
+            //    .Where(item => item % 2 > 0)
+            //    .ToArray()
+            //);
+
+            txtResult.Text = string.Join(" ", txtData
+                .Text
+                .Split(sep)
+                .Select(item => Convert.ToInt32(item))
+                .OrderByDescending(item => item)
+                .ToArray()
+            );
+
+            // стиль запросы
+            // стиль методы
+        }
+    }
+}
