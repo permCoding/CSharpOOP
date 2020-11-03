6e1c0bc7dada1c27ebd25ac938b9c9a12b409501
Андрей Беляков <ttxiom@gmail.com>
2020-11-03 10:47:31 +0500

    WebClient

---
+++ WebClientTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClientTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            WebClient webClient = new WebClient();
+            string s = webClient.DownloadString("https://pcoding.ru/csv/00.txt");
+            
+            Console.WriteLine(s);
+            
+            Console.ReadLine();
+        }
+    }
+}
