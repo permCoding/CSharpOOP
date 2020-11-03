28745743d86bbbdace110dab2eaa5fa43cff1a48
Андрей Беляков <ttxiom@gmail.com>
2020-11-03 13:05:09 +0500

    WebClient

--- WebClientTest/Program.cs
+++ WebClientTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -12,10 +13,29 @@ namespace WebClientTest
         static void Main()
         {
             WebClient webClient = new WebClient();
-            string s = webClient.DownloadString("https://pcoding.ru/csv/00.txt");
-            
+            string s = webClient.DownloadString("https://pcoding.ru/csv/04.txt");
+
             Console.WriteLine(s);
-            
+
+
+
+            #region примеры обработки чисел с точкой            
+            //Console.WriteLine(double.Parse("1,5") + 0.10);
+
+            //Console.WriteLine(double.Parse("1.5", CultureInfo.InvariantCulture) + 0.10);
+
+            //System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            //Console.WriteLine(double.Parse("1.5") + 0.10);
+            //Console.WriteLine(double.Parse("1.5", CultureInfo.InvariantCulture) + 0.10);
+            //Console.WriteLine(double.Parse("1.5", CultureInfo.CurrentCulture) + 0.10);
+
+            //string number = "1.5";
+            //Console.WriteLine(double.Parse(number, CultureInfo.CurrentCulture) + 0.10);
+            //System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+            //Console.WriteLine(double.Parse("1,5") + 0.10);
+            ////Console.WriteLine(double.Parse("1.5") + 0.10);
+            #endregion
+
             Console.ReadLine();
         }
     }
