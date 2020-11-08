5fa50ea1a988d7ea1789bee675f9e248dd14b077
Андрей Беляков <ttxiom@gmail.com>
2020-11-08 23:07:20 +0500

    linq_labrab

---
+++ LinqLabRab/Module.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqLabRab
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string NameStudent { get; set; }
+        public char Sex { get; set; }
+        public int BallMath { get; set; }
+        public int BallLang { get; set; }
+        public int BallInf { get; set; }
+
+        public int IdDirect { get; set; }
+        public int Balls => BallInf + BallLang + BallMath;
+        public Student(params string[] attr)
+        {
+            IdStudent = int.Parse(attr[0]);
+            NameStudent = attr[1];
+            Sex = Convert.ToChar(attr[2]);
+            BallMath = int.Parse(attr[3]);
+            BallLang = int.Parse(attr[4]);
+            BallInf = int.Parse(attr[5]);
+            IdDirect = int.Parse(attr[6]);
+        }
+    }
+    public class Direct
+    {
+        public int IdDirect { get; set; }
+        public string NameDirect { get; set; }
+        public int MinBall { get; set; }
+        public int Limit { get; set; }
+        public Direct(params string[] attr)
+        {
+            IdDirect = int.Parse(attr[0]);
+            NameDirect = attr[1];
+            MinBall = int.Parse(attr[2]);
+            Limit = int.Parse(attr[3]);
+        }
+    }
+}
---
+++ LinqLabRab/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLabRab
+{
+    class Program
+    {
+        static void task_8_GroupJoin()
+        {
+            var students = File
+                .ReadAllLines("students.txt")
+                .Skip(1)
+                .Select(line => new Student(line.Split(';')));
+
+            var directions = File
+                .ReadAllLines("directions.txt")
+                .Skip(1)
+                .Select(line => new Direct(line.Split(';')));
+
+            var filter = directions
+                .GroupJoin(
+                    students,
+                    d => d.IdDirect,
+                    s => s.IdDirect,
+                    (d, s) => new { 
+                        ND = d.NameDirect, 
+                        AVG = (double)s
+                            .Select(item => item.Balls)
+                            .OrderByDescending(item => item)
+                            .Take(d.Limit)
+                            .Sum() / d.Limit 
+                    }
+                )
+                .OrderByDescending(item => item.AVG);
+
+            foreach (var item in filter)
+                Console.WriteLine($"{item.AVG:f2} {item.ND}");
+        }
+        static void task_8_Join_GroupBy()
+        {
+            var students = File
+                .ReadAllLines("students.txt")
+                .Skip(1)
+                .Select(line => new Student(line.Split(';')));
+
+            var directions = File
+                .ReadAllLines("directions.txt")
+                .Skip(1)
+                .Select(line => new Direct(line.Split(';')));
+
+            var groups = directions
+                .Join(
+                    students,
+                    d => d.IdDirect,
+                    s => s.IdDirect,
+                    (d, s) => new { s.Balls, d.IdDirect }
+                )
+                .GroupBy(item => item.IdDirect);
+
+            var for_direct = directions
+                .Join(
+                    groups,
+                    d => d.IdDirect,
+                    g => g.Key,
+                    (d, g) => new { 
+                        ND = d.NameDirect, 
+                        AVG = (double)g
+                            .Select(item => item.Balls)
+                            .OrderByDescending(item => item)
+                            .Take(d.Limit)
+                            .Sum() / d.Limit
+                    }
+                )
+                .OrderByDescending(item => item.AVG);
+
+            foreach (var item in for_direct)
+                Console.WriteLine($"{item.AVG:f2} {item.ND}");
+        }
+        static void task_8_SelectMany()
+        {
+            var students = File
+                .ReadAllLines("students.txt")
+                .Skip(1)
+                .Select(line => new Student(line.Split(';')));
+
+            var directions = File
+                .ReadAllLines("directions.txt")
+                .Skip(1)
+                .Select(line => new Direct(line.Split(';')));
+
+            var groups = directions
+                .SelectMany(d => students
+                    .Select(s => new { s.Balls, s.IdDirect })
+                    .Where(item => item.IdDirect == d.IdDirect)
+                )
+                .GroupBy(item => item.IdDirect);
+
+            //foreach (var group in groups)
+            //{
+            //    Console.WriteLine(group.Key);
+            //    foreach (var item in group.Select(item => item.Balls))
+            //        Console.WriteLine(item);
+            //}
+
+            var for_direct = directions
+                .Join(
+                    groups,
+                    d => d.IdDirect,
+                    g => g.Key,
+                    (d, g) => new {
+                        ND = d.NameDirect,
+                        AVG = (double)g
+                            .Select(item => item.Balls)
+                            .OrderByDescending(item => item)
+                            .Take(d.Limit)
+                            .Sum() / d.Limit
+                    }
+                )
+                .OrderByDescending(item => item.AVG);
+
+            foreach (var item in for_direct)
+                Console.WriteLine($"{item.AVG:f2} {item.ND}");
+        }
+
+        static void Main(string[] args)
+        {
+            task_8_GroupJoin();
+            Console.WriteLine();
+            task_8_Join_GroupBy();
+            Console.WriteLine();
+            task_8_SelectMany();
+            Console.ReadLine();
+        }
+    }
+}
