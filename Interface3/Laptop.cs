2c58a912e34dc595b521b5439b0bc87f70df9e05
Андрей Беляков <ttxiom@gmail.com>
2020-12-01 16:49:12 +0500

    end

---
+++ Interface3/Laptop.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface3
+{
+    public interface IScreen
+    {
+        int Colors { get; set; }
+        int Width { get; set; }
+        int Height { get; set; }
+        void PrintInfo(Bitmap bmp);
+    }
+    public class Laptop
+    {
+        public string Name { get; set; }
+        public IScreen screen;
+        public Keyboard keyboard;
+        public Laptop(string name)
+        {
+            Name = name;
+        }
+    }
+    public class Screen: IScreen
+    {
+        public int Colors { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public void PrintInfo(Bitmap bmp)
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+            // масштабировать - 2
+            // - 3 - мб про интерфесы клавы
+            Console.WriteLine($"{w}: {h}");
+        }
+    }
+    public class ScreenRotate: IScreen
+    {
+        public bool Direction { get; set; }
+        public int Colors { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public void PrintInfo(Bitmap bmp)
+        {
+            int w = bmp.Width;
+            int h = bmp.Height;
+            if (Direction) { }; // повернуть картинку - 1
+            // масштабировать - 2
+            // - 3
+            Console.WriteLine($"{w}: {h}");
+        }
+    }
+    public class Keyboard
+    {
+
+    }
+}
--- Interface3/Program.cs
+++ Interface3/Program.cs
@@ -14,8 +14,23 @@ namespace Interface3
             // свойства класса реализованы на других классах (не наследование)
             #endregion
 
+            Laptop lt1 = new Laptop("HP x212");
+            // все параметры передавать через конструктор
+            lt1.screen = new Screen();
+            lt1.screen.Colors = 32;
+            lt1.screen.Width = 1920;
+            lt1.screen.Height = 1080;
 
+            lt1.screen.PrintInfo(Properties.Resources.algo);
 
+            Laptop lt2 = new Laptop("Acer e14");
+            // все параметры передавать через конструктор
+            lt2.screen = new ScreenRotate();
+            lt2.screen.Colors = 32;
+            lt2.screen.Width = 1920;
+            lt2.screen.Height = 1080;            
+            // Direction - false - true
+            lt2.screen.PrintInfo(Properties.Resources.algo);
 
             Console.ReadLine();
         }
