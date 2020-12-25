d2d7116a05a0b23f66e710a90d60e62a61e38496
Андрей Беляков <ttxiom@gmail.com>
2020-12-25 22:37:04 +0500

    up

---
+++ InterView1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace InterView1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string link = "https://pcoding.ru/csv/04.txt";
+
+            WebClient webClient = new WebClient();
+            string s = webClient.DownloadString(link);
+            Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
