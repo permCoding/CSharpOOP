75a1d72060b15489df9c5bc11feeacc20bc740c1
Андрей Беляков <ttxiom@gmail.com>
2020-12-26 16:39:39 +0500

    up

--- InterView1/Program.cs
+++ InterView1/Program.cs
@@ -11,15 +11,44 @@ namespace InterView1
     class Example
     {
         private string link;
+        private static WebClient webClient = new WebClient();
+        public string Link
+        {
+            //get { return link.Split('/').Last(); }
+            get { return link; }
+            set
+            {
+                if (value.StartsWith("http"))
+                {
+                    link = value;
+                }
+                else
+                {
+                    link = "https://pcoding.ru/csv/03.txt";
+                    Console.WriteLine("Ссылка неверна");
+                }
+            }
+        }
+
         public string reader()
         {
-            WebClient webClient = new WebClient();
-            string s = webClient.DownloadString(link);
-            return s;
+            return webClient.DownloadString(Link);
+        }
+        public string reader(string _link)
+        {   
+            return  webClient.DownloadString(_link);
+        }
+        public Example()
+        {
+            
         }
         public Example(string link)
         {
-            this.link = link;
+            this.Link = link;
+        }
+        public Example(int num)
+        {
+            this.Link = "https://pcoding.ru/csv/0"+ num.ToString() +".txt";
         }
     }
     class Program
@@ -28,9 +57,20 @@ namespace InterView1
         {
             string link = "https://pcoding.ru/csv/04.txt";
 
-            Example example1 = new Example(link);
-            Console.WriteLine(example1.reader());
-            
+            //Example example1 = new Example(link);
+            //example1.Link = "htps://pcoding.ru/csv/03.txt";
+
+            //Example example2 = new Example(link);
+
+            Example example3 = new Example(6);
+            Console.WriteLine(example3.reader());
+            //Console.WriteLine(example1.reader());
+            //Console.WriteLine(example2.reader());
+
+            //Console.WriteLine(example1.Link);
+            //Console.WriteLine(example2.Link);
+
+
             Console.ReadLine();
         }
     }
