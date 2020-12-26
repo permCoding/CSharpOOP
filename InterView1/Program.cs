3fec5444917a617b0b53f7624487811bbe6a40ea
Андрей Беляков <ttxiom@gmail.com>
2020-12-26 15:19:19 +0500

    up

--- InterView1/Program.cs
+++ InterView1/Program.cs
@@ -8,16 +8,29 @@ using System.IO;
 
 namespace InterView1
 {
+    class Example
+    {
+        private string link;
+        public string reader()
+        {
+            WebClient webClient = new WebClient();
+            string s = webClient.DownloadString(link);
+            return s;
+        }
+        public Example(string link)
+        {
+            this.link = link;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             string link = "https://pcoding.ru/csv/04.txt";
 
-            WebClient webClient = new WebClient();
-            string s = webClient.DownloadString(link);
-            Console.WriteLine(s);
-
+            Example example1 = new Example(link);
+            Console.WriteLine(example1.reader());
+            
             Console.ReadLine();
         }
     }
